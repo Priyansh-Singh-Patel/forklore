32907b746ea3b442c349a261fa7081a1bbf9f67a
Priyansh-Singh-Patel <priyansh2004singh@gmail.com>
2025-02-17 03:33:36 +0530

    home fragment

--- app/src/main/java/com/example/forklore/Fragment/HomeFragment.kt
+++ app/src/main/java/com/example/forklore/Fragment/HomeFragment.kt
@@ -6,9 +6,11 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.denzcoskun.imageslider.constants.ScaleTypes
 import com.denzcoskun.imageslider.interfaces.ItemClickListener
 import com.denzcoskun.imageslider.models.SlideModel
+import com.example.forklore.Adapter.popularAdapter
 import com.example.forklore.R
 import com.example.forklore.databinding.FragmentHomeBinding
 
@@ -55,6 +57,13 @@ class HomeFragment : Fragment() {
                 Toast.makeText(requireContext(),itemMessage,Toast.LENGTH_SHORT).show()
             }
         })
+
+        val foodName = listOf("Burger","Sandwich","MoMo","Chat")
+        val Price = listOf("₹250","₹100","₹80","₹50")
+        val popularFoodImages = listOf(R.drawable.menu1, R.drawable.menu2, R.drawable.menu3, R.drawable.menu4)
+        val adapter = popularAdapter(foodName,Price,popularFoodImages)
+        binding.PopularRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+        binding.PopularRecyclerView.adapter = adapter
     }
 
     companion object {
