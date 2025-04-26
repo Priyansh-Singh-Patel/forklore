ed2203a1a647a1f4b38c39d0ca39f4f4b23242ec
Priyansh-Singh-Patel <priyansh2004singh@gmail.com>
2025-04-26 23:31:08 +0530

    fragment

---
+++ app/src/main/java/com/example/forklore/Fragment/HistoryFragment.kt
@@ -0,0 +1,45 @@
+package com.example.forklore.Fragment
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.example.forklore.Adapter.BuyAgainAdapter
+import com.example.forklore.R
+import com.example.forklore.databinding.FragmentHistoryBinding
+
+class HistoryFragment : Fragment() {
+
+    private lateinit var binding: FragmentHistoryBinding
+    private lateinit var buyAgainAdapter: BuyAgainAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentHistoryBinding.inflate(layoutInflater,container,false)
+        // Inflate the layout for this fragment
+        setupRecyclerView()
+        return binding.root
+    }
+
+    private fun setupRecyclerView() {
+        val buyAgainFoodName = arrayListOf("Food 1", "Food 2", "Food 3")
+        val buyAgainFoodPrice = arrayListOf("₹50", "₹80", "₹75")
+        val buyAgainFoodImage = arrayListOf(R.drawable.menu1, R.drawable.menu4,R.drawable.menu1)
+
+        buyAgainAdapter = BuyAgainAdapter(buyAgainFoodName, buyAgainFoodPrice, buyAgainFoodImage)
+        binding.BuyAgainRecyclerView.adapter = buyAgainAdapter
+        binding.BuyAgainRecyclerView.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    companion object {
+
+    }
+}
