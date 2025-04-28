45e7a7b4d3e765972e56146e05402db4805a8424
Priyansh-Singh-Patel <priyansh2004singh@gmail.com>
2025-04-28 23:57:45 +0530

    fragment

--- app/src/main/java/com/example/forklore/MainActivity.kt
+++ app/src/main/java/com/example/forklore/MainActivity.kt
@@ -13,11 +13,10 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bottomNavigationView)) { view, insets ->
+            val navInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            view.setPadding(0, 0, 0, navInsets.bottom)  // Set the bottom padding manually
             insets
         }
 
