fbc674f39fdd5267f4c43603aa36a56c1dd2aeed
Priyansh-Singh-Patel <priyansh2004singh@gmail.com>
2025-05-07 18:23:54 +0530

    notification adapter

---
+++ app/src/main/java/com/example/forklore/Adapter/NotificationAdapter.kt
@@ -0,0 +1,22 @@
+package com.example.forklore.Adapter
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class NotificationAdapter(): RecyclerView.Adapter<NotificationAdapter.NotificationViewHolder>() {
+    inner class NotificationViewHolder {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotificationViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: NotificationViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
