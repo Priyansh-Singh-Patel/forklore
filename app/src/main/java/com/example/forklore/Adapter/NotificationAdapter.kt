baa104f9ec887e433d44de6d9f81fcf9a3f2fd9b
Priyansh-Singh-Patel <priyansh2004singh@gmail.com>
2025-05-14 23:38:31 +0530

    notification adapter

--- app/src/main/java/com/example/forklore/Adapter/NotificationAdapter.kt
+++ app/src/main/java/com/example/forklore/Adapter/NotificationAdapter.kt
@@ -1,22 +1,25 @@
 package com.example.forklore.Adapter
 
+import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.example.forklore.databinding.FragmentNotificationBottomBinding
+import com.example.forklore.databinding.NotificationItemBinding
 
-class NotificationAdapter(): RecyclerView.Adapter<NotificationAdapter.NotificationViewHolder>() {
-    inner class NotificationViewHolder {
-
-    }
+class NotificationAdapter(private var notification: ArrayList<String>, private var notificationImage: ArrayList<Int>): RecyclerView.Adapter<NotificationAdapter.NotificationViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NotificationViewHolder {
-        TODO("Not yet implemented")
-    }
-
-    override fun getItemCount(): Int {
-        TODO("")
+        val binding = NotificationItemBinding.inflate(LayoutInflater.from(parent.context),parent,false)
+        return NotificationViewHolder(binding)
     }
 
     override fun onBindViewHolder(holder: NotificationViewHolder, position: Int) {
         TODO("")
+    }
+
+    override fun getItemCount(): Int = notification.size
+
+    inner class NotificationViewHolder(private val binding: NotificationItemBinding): RecyclerView.ViewHolder(binding.root) {
+
     }
 }
