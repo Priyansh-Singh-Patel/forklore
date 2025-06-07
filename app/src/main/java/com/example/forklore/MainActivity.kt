964f58e4dbf1e9b42f96b2a5ebd6ecafad1a64b3
Priyansh-Singh-Patel <priyansh2004singh@gmail.com>
2025-06-07 23:33:24 +0530

    search activity

--- app/src/main/java/com/example/forklore/MainActivity.kt
+++ app/src/main/java/com/example/forklore/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.forklore
 
 import android.os.Bundle
+import android.view.LayoutInflater
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -8,12 +9,17 @@ import androidx.core.view.WindowInsetsCompat
 import androidx.navigation.NavController
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
+import com.example.forklore.databinding.ActivityMainBinding
+import com.example.forklore.databinding.NotificationItemBinding
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.bottomNavigationView)) { view, insets ->
             val navInsets = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             view.setPadding(0, 0, 0, navInsets.bottom)  // Set the bottom padding manually
@@ -23,6 +29,11 @@ class MainActivity : AppCompatActivity() {
         var NavController = findNavController(R.id.fragmentContainerView)
         var bottomnav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomnav.setupWithNavController(NavController)
+
+        binding.notificationButton.setOnClickListener {
+            val bottomSheetDialog = Notification_Bottom_Fragment()
+            bottomSheetDialog.show(supportFragmentManager,"Test")
+        }
 
     }
 }
