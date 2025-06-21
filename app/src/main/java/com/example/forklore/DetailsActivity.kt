777d560807116a4e6f25a5762830a8008ef55f16
Priyansh-Singh-Patel <priyansh2004singh@gmail.com>
2025-06-21 23:53:06 +0530

    minor changes

---
+++ app/src/main/java/com/example/forklore/DetailsActivity.kt
@@ -0,0 +1,34 @@
+package com.example.forklore
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.forklore.databinding.ActivityDetailsBinding
+
+class DetailsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityDetailsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityDetailsBinding.inflate(layoutInflater)
+        enableEdgeToEdge()
+        setContentView(binding.root)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val foodName = intent.getStringExtra("MenuItemName")
+        val foodImage = intent.getIntExtra("MenuItemImage", 0)
+        binding.detailFoodName.text = foodName
+        binding.detailFoodImage.setImageResource(foodImage)
+
+        binding.imageButton1.setOnClickListener {
+            finish()
+        }
+    }
+}
